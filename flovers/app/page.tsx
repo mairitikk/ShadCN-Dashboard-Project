f149f0b611f3c93ac5b3ef64f4fbb03a2bca53e3
mairi tikk <mairi.tikk@gmail.com>
2025-05-20 09:48:16 +0200

    add navbar

--- flovers/app/page.tsx
+++ flovers/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "../components/ui/button"
+import {CirclePlus} from "lucide-react"
 
 const Homepage = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Button variant={"ghost"}>Vajuta siia</Button>
+      <Button variant="outline" size="lg">
+        <CirclePlus />
+        Vajuta siia</Button>
     </div>
   )
 }
