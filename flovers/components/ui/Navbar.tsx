05a05a161083674ddacafde173970993ddc571da
mairi tikk <mairi.tikk@gmail.com>
2025-05-20 10:49:07 +0200

    menu changes

--- flovers/components/ui/Navbar.tsx
+++ flovers/components/ui/Navbar.tsx
@@ -1,6 +1,18 @@
 import Link from "next/link"
 import { Moon } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { User } from "lucide-react"
+import { Settings } from "lucide-react"
+import { LogOut } from "lucide-react"
+
 
 
 const Navbar = () => {
@@ -12,10 +24,31 @@ const Navbar = () => {
             <div className="flex items-center gap-4">
             <Link href="/">Dashboard</Link>
                 <Moon></Moon>
-                <Avatar>
+               
+                <DropdownMenu>
+  <DropdownMenuTrigger> <Avatar>
   <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" />
   <AvatarFallback>CN</AvatarFallback>
-</Avatar>
+                    </Avatar>
+                    </DropdownMenuTrigger>
+  <DropdownMenuContent sideOffset={10}>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <User className="h-[1.2rem] w-[1.2rem] mr-2"></User>
+                            Profile
+    </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Settings className="h-[1.2rem] w-[1.2rem] mr-2"></Settings>
+                            Settings
+    </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <LogOut className="h-[1.2rem] w-[1.2rem] mr-2"></LogOut>
+                            Logout</DropdownMenuItem>
+    
+  </DropdownMenuContent>
+</DropdownMenu>
+
 
 
             </div>
