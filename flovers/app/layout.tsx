66e59e2c891e2f5a1eeaaf250dee07539af427ee
mairi tikk <mairi.tikk@gmail.com>
2025-05-20 09:54:24 +0200

    layout changes

--- flovers/app/layout.tsx
+++ flovers/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "../components/ui/Sidebar";
+import Navbar from "@/components/ui/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Sidebar></Sidebar>
+        <main className="">
+          <Navbar></Navbar>
+          {children}
+        </main>
+        
       </body>
     </html>
   );
