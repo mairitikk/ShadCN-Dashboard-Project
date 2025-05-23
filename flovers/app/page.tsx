76a35c0d666f99f3874ef32596a8f48e0005704c
mairi tikk <mairi.tikk@gmail.com>
2025-05-23 10:40:36 +0200

    add test

--- flovers/app/page.tsx
+++ flovers/app/page.tsx
@@ -2,7 +2,9 @@
 
 const Homepage = () => {
   return (
-   <div></div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4, gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+    </div>
   )
 }
 export default Homepage
