2a5a2bcc8c97abf6db2aad365018e3c1de2be95c
mairi tikk <mairi.tikk@gmail.com>
2025-05-26 12:17:56 +0200

    add html

--- flovers/components/AppBarChart.tsx
+++ flovers/components/AppBarChart.tsx
@@ -1,5 +1,7 @@
 "use client"
-import { type ChartConfig } from "@/components/ui/chart"
+import { ChartContainer, type ChartConfig } from "@/components/ui/chart"
+import { BarChart } from "recharts"
+import { Bar } from "recharts"
 
 const chartConfig = {
   desktop: {
@@ -23,7 +25,15 @@ const chartData = [
 
 const AppBarChart = () => {
     return (
-        <div className="">AppBarChart</div>
+        <div className="">
+            <h1>Total Revenue</h1>
+             <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+      <BarChart accessibilityLayer data={chartData}>
+        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+      </BarChart>
+    </ChartContainer>
+        </div>
     )
 }
 export default AppBarChart
