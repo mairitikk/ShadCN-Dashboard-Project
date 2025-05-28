ceff41f8a02a183f973b4e145427f7b5a978d076
mairi tikk <mairi.tikk@gmail.com>
2025-05-28 11:06:20 +0200

    add in page CardList

--- flovers/app/page.tsx
+++ flovers/app/page.tsx
@@ -1,15 +1,16 @@
 import AppAreaChart from "@/components/AppAreaChart"
 import AppBarChart from "@/components/AppBarChart"
 import AppPieChart from "@/components/AppPieChart"
+import CardList from "@/components/CardList"
 
 
 const Homepage = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 2xl:col-span-2"><AppBarChart></AppBarChart></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><CardList></CardList></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart></AppPieChart></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><CardList></CardList></div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 2xl:col-span-2"><AppAreaChart></AppAreaChart></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
     </div>
--- flovers/components/CardList.tsx
+++ flovers/components/CardList.tsx
@@ -1,4 +1,6 @@
-import { Card } from "./ui/card";
+import Image from "next/image";
+import { Card, CardContent, CardFooter, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
 
 const popularContent = [
   {
@@ -85,18 +87,34 @@ const latestTransactions = [
     count: 1400,
   },
 ];
-const CardList = ({ title }: { title: string }) => {
-    const list = title === "Popilar Content" ? popularContent : latestTransactions
-    return (
-        <div className="">
-            <h1 className="text-lg font-medium mb-6">{title}</h1>
-            <div className="flex flex-col gap-2">
-                {list.map(item => (
-                    <Card key={item.id}></Card>
-                ))}
-            </div>
-        </div>
-    )
-}
 
-export default CardList
+const CardList = ({ title }: { title: string }) => {
+  const list =
+    title === "Popular Content" ? popularContent : latestTransactions;
+  return (
+    <div className="">
+      <h1 className="text-lg font-medium mb-6">{title}</h1>
+      <div className="flex flex-col gap-2">
+        {list.map((item) => (
+          <Card key={item.id} className="flex-row items-center justify-between gap-4 p-4">
+            <div className="w-12 h-12 rounded-sm relative overflow-hidden">
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <CardContent className="flex-1 p-0">
+              <CardTitle className="text-sm font-medium">{item.title}</CardTitle>
+              <Badge variant="secondary">{item.badge}</Badge>
+            </CardContent>
+            <CardFooter className="p-0">{item.count / 1000}K</CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
