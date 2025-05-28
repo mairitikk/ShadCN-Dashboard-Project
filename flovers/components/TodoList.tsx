22fee0ea1f817afb19227eaedbf9ce9e10eb82b9
mairi tikk <mairi.tikk@gmail.com>
2025-05-28 12:10:55 +0200

    add checkbox

--- flovers/components/TodoList.tsx
+++ flovers/components/TodoList.tsx
@@ -1,11 +1,20 @@
 import { ScrollArea } from "@radix-ui/react-scroll-area"
+import { Card } from "./ui/card"
+import { Checkbox } from "@radix-ui/react-checkbox"
 
 const TodoList = () => {
     return (
         <div className="">
           Calender
             {/*LIST*/}
-            <ScrollArea className="max-h-[400px]"></ScrollArea>
+            <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
+                {/*LIST ITEM*/}
+                <Card>
+                    <div className="flex items-center gap-4">
+                        <Checkbox></Checkbox>
+                    </div>
+                </Card>
+            </ScrollArea>
         </div>
     )
 }
