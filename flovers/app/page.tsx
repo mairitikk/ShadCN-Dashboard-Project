5082d4b447c7359bfee987421b347b3006742627
mairi tikk <mairi.tikk@gmail.com>
2025-05-28 11:11:39 +0200

    nextt config file chanegs

--- flovers/app/page.tsx
+++ flovers/app/page.tsx
@@ -8,11 +8,11 @@ const Homepage = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 2xl:col-span-2"><AppBarChart></AppBarChart></div>
-      <div className="bg-primary-foreground p-4 rounded-lg"><CardList></CardList></div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Lates Transaction"></CardList></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart></AppPieChart></div>
-      <div className="bg-primary-foreground p-4 rounded-lg"><CardList></CardList></div>
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 2xl:col-span-2"><AppAreaChart></AppAreaChart></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 2xl:col-span-2"><AppAreaChart></AppAreaChart></div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Popular Content"></CardList></div>
     </div>
   )
 }
--- flovers/next.config.ts
+++ flovers/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  imagess: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
