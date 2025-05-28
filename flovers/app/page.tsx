2699d5751d4658d17ca290bfe363dd929b5b6eb7
mairi tikk <mairi.tikk@gmail.com>
2025-05-28 09:18:41 +0200

    add PieChard

--- flovers/app/page.tsx
+++ flovers/app/page.tsx
@@ -1,5 +1,6 @@
 import AppAreaChart from "@/components/AppAreaChart"
 import AppBarChart from "@/components/AppBarChart"
+import AppPieChart from "@/components/AppPieChart"
 
 
 const Homepage = () => {
@@ -7,7 +8,7 @@ const Homepage = () => {
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 2xl:col-span-2"><AppBarChart></AppBarChart></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart></AppPieChart></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 2xl:col-span-2"><AppAreaChart></AppAreaChart></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
